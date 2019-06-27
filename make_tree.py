61dd1eefbb7b0ece0057b7a24fbf69c9adacc3cf
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2019-06-27 14:03:02 -0600

    Update make_tree.py

--- make_tree.py
+++ make_tree.py
@@ -3,6 +3,7 @@ import csv
 
 # EXTERNAL LIBRARIES
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 from scipy.cluster import hierarchy
 from scipy.spatial.distance import squareform
@@ -17,19 +18,31 @@ def make_tree(matrix, labels):
     sf2 = hierarchy.linkage(sf, 'single')
 
     hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
-    fig, ax = plt.subplots(figsize=(8, 3))
+    fig, ax = plt.subplots(figsize=(12, 10))
     hierarchy.dendrogram(sf2, ax=ax, labels=labels, orientation='left')
 
-    hierarchy.set_link_color_palette(None)  # reset to default after use
+    matplotlib.rc('xtick', labelsize=8)
+    matplotlib.rc('ytick', labelsize=8)
 
-    plt.show()
+    hierarchy.set_link_color_palette(None)  # reset to default after use
+    fig.tight_layout()
+    fig.savefig("dendrogram.png", bbox_inches='tight', dpi=400)
+
+
+def get_labels(file):
+    with open(file, 'r') as fh:
+        labels = fh.readline().rstrip().split(',')
+        labels = [label.replace("Mycobacterium", "M.") for label in labels]
+        return np.array(labels)
 
 
 if __name__ == "__main__":
     # YOU SET THESE:
-    labels_file = ""
-    distance_file = ""
+    labels_file = "sp_names.csv"
+    distance_file = "Difference_matrix.csv"
 
-    labels = np.genfromtxt(labels_file, delimiter=',')
+    labels = get_labels(labels_file)
+    print(labels)
     matrix = np.genfromtxt(distance_file, delimiter=',')
     make_tree(matrix, labels)
+
