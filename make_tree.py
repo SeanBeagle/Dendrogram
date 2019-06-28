f3105da6ba4dfc1283eaf893e853e10aa2bc2ab6
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2019-06-28 06:24:51 -0600

    Update make_tree.py

--- make_tree.py
+++ make_tree.py
@@ -1,48 +1,41 @@
-# PYTHON STANDARD LIBRARY
-import csv
-
-# EXTERNAL LIBRARIES
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 from scipy.cluster import hierarchy
 from scipy.spatial.distance import squareform
 
 
-def make_tree(matrix, labels):
-    """Return dendrogram of sequences."""
+def make_tree(matrix, labels, threshold=None, filename="dendrogram.png", figsize=(15, 10)):
+    """Return dendrogram from distance matrix and labels."""
+    print("Making dendrogram...")
 
-    labels = labels
-
+    # FORMAT MATRIX AS VECTOR
     sf = squareform(matrix, force='tovector')
     sf2 = hierarchy.linkage(sf, 'single')
 
-    hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
-    fig, ax = plt.subplots(figsize=(12, 10))
-    hierarchy.dendrogram(sf2, ax=ax, labels=labels, orientation='left')
-
-    matplotlib.rc('xtick', labelsize=8)
-    matplotlib.rc('ytick', labelsize=8)
-
-    hierarchy.set_link_color_palette(None)  # reset to default after use
+    # CREATE FIGURE
+    fig, ax = plt.subplots(figsize=figsize)
     fig.tight_layout()
-    fig.savefig("dendrogram.png", bbox_inches='tight', dpi=400)
+
+    # ADD DENDROGRAM TO FIGURE
+    with plt.rc_context({'lines.linewidth': 0.5}):
+        hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
+        hierarchy.dendrogram(sf2, ax=ax, labels=labels, orientation='left', color_threshold=threshold)
+
+    # SAVE FIGURE
+    fig.savefig(filename, bbox_inches='tight', dpi=600)
+
+    print(f"...Saved dendrogram as: {filename}")
 
 
-def get_labels(file):
-    with open(file, 'r') as fh:
-        labels = fh.readline().rstrip().split(',')
-        labels = [label.replace("Mycobacterium", "M.") for label in labels]
-        return np.array(labels)
+def get_labels(file, delimiter=','):
+    """Get labels from delimited file"""
+    return np.array([label for label in open(file, 'r').readline().rstrip().split(delimiter)])
 
 
 if __name__ == "__main__":
-    # YOU SET THESE:
-    labels_file = "sp_names.csv"
-    distance_file = "Difference_matrix.csv"
+    labels_csv = "sp_names.csv"
+    matrix_csv = "Difference_matrix.csv"
 
-    labels = get_labels(labels_file)
-    print(labels)
-    matrix = np.genfromtxt(distance_file, delimiter=',')
-    make_tree(matrix, labels)
-
+    labels = get_labels(labels_csv)
+    matrix = np.genfromtxt(matrix_csv, delimiter=',')
+    make_tree(matrix=matrix, labels=labels, threshold=3, figsize=(15, 10))
